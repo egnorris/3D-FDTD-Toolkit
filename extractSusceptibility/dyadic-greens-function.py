1296bda89192f34c71326e2b0d82fffb52bbdefd
egnorris <evangnorris@gmail.com>
2026-05-14 16:34:25 -0400

    dyadic green's function implemented

--- extractSusceptibility/dyadic-greens-function.py
+++ extractSusceptibility/dyadic-greens-function.py
@@ -11,7 +11,7 @@ def drawBound(ax, xDom, yDom, xMin=0, yMin=0, xB=20, yB=20, c='red', l='', z=-2)
     ax.fill_betweenx([yMin, yDom], [xDom+xB, xDom+xB],[xDom, xDom], color=c, zorder=z, alpha=0.5, ec='none')
 
 
-def visualizeCoordinates(r, r0, d):
+def visualize_coordinates(r, r0, d):
     x, y, z = r
     x0, y0, z0 = r0
     fig, axs = plt.subplots(2, 1)
@@ -42,20 +42,44 @@ def visualizeCoordinates(r, r0, d):
     plt.tight_layout()
     plt.savefig("PositionDiagram.png", dpi =200)
 
-def scalar_greens_function(wavelength,R,R0, verbose=False):
-    
+
+def scalar_greens_function(wavelength,R):
     k = 2*np.pi/wavelength
-    temp = np.abs(np.asarray(R) - np.asarray(R0))
-    normed = np.linalg.norm(temp)
-    G0 = np.exp(1j*k*normed)/(4*np.pi*normed)
-    if verbose == True:
-        print("\nScalar Green's Function")
-        print(f"r0: ({R0[0]}nm, {R0[1]}nm, {R0[2]}nm)")
-        print(f"r: ({R[0]}nm, {R[1]}nm, {R[2]}nm) ")
-        print(f"r-r0: ({temp[0]}nm ,{temp[1]}nm, {temp[2]}nm)")
-        print(f"|r-r0|: {normed}nm ")
-        print(f"G0: ({G0:.2e}) nm^-1\n")
+    G0 = np.exp(1j*k*R)/(4*np.pi*R)
     return G0
+
+def dyadic_greens_function(wavelength, r, r0, verbose=False):
+    r_diff = np.asarray(r) - np.asarray(r0)
+    R = np.linalg.norm(np.abs(r_diff))
+    RR = np.outer(r_diff, r_diff)
+    k = 2*np.pi/wavelength
+    if verbose == True:
+        print(f"\nr0:             ({r0[0]}nm, {r0[1]}nm, {r0[2]}nm)")
+        print(f"r:              ({r[0]}nm, {r[1]}nm, {r[2]}nm) ")
+        print(f"r-r0:           ({r_diff[0]}nm ,{r_diff[1]}nm, {r_diff[2]}nm)")
+        print(f"||r-r0||:       {R}nm ")
+        print(f"||r-r0||^2:     {R**2:.2e} nm^2 ")
+        print(f"                |{RR[0,0]:.2e} nm^2, {RR[0,1]:.2e} nm^2, {RR[0,2]:.2e} nm^2|")
+        print(f"RR:             |{RR[1,0]:.2e} nm^2, {RR[1,1]:.2e} nm^2, {RR[1,2]:.2e} nm^2|")
+        print(f"                |{RR[2,0]:.2e} nm^2, {RR[2,1]:.2e} nm^2, {RR[2,2]:.2e} nm^2|")
+        print(f"\nwavelength:     {wavelength}nm")
+        print(f"wavenumber:     {k:.2e} nm^-1")
+    G0 = scalar_greens_function(wavelength,R)
+    kR = k*R
+    k2R2 = kR*kR
+    ikR = 1j*kR
+    a = np.exp(ikR)/(4*np.pi*R)
+    b = 1 + (ikR-1)/(k2R2)
+    c = (3-3*ikR-k2R2)/(k2R2)
+    I = np.array([[1,0,0],[0,1,0],[0,0,1]])
+    G = a*(b*I + c*RR)
+    if verbose == True:
+        print(f"\nG0:             ({G0:.2e}) nm^-1\n")
+        print(f"                |({G[0,0]:.2e}) nm^-1, ({G[0,1]:.2e}) nm^-1, ({G[0,2]:.2e}) nm^-1|")
+        print(f"G:              |({G[1,0]:.2e}) nm^-1, ({G[1,1]:.2e}) nm^-1, ({G[1,2]:.2e}) nm^-1|")
+        print(f"                |({G[2,0]:.2e}) nm^-1, ({G[2,1]:.2e}) nm^-1, ({G[2,2]:.2e}) nm^-1|")
+    
+    return G
 
 
 
@@ -73,5 +97,5 @@ if __name__=="__main__":
     R0 = [x0,y0,z0]
     R = [x,y,z]
 
-    visualizeCoordinates(R, R0, domain)
-    scalar_greens_function(800,R,R0, verbose=True)
+    visualize_coordinates(R, R0, domain)
+    dyadic_greens_function(800, R, R0, verbose=True)
