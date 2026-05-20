461828677ab1753b589237f433596ce06dc62aad
egnorris <evangnorris@gmail.com>
2026-05-20 11:05:59 -0400

    get_dft_file() is able to successfully search simDIR/OUTPUT/DFT for monitor data at wavelengths nearest to those specified by input arguments

--- readDFTMonitor/readDFT.py
+++ readDFTMonitor/readDFT.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import numpy as np
+import os
 
 def read_monitor_info(simDIR, v=False):
     """
@@ -14,14 +15,19 @@ def read_monitor_info(simDIR, v=False):
     #ini_fname = f"{simDIR}/INIDEF/pphinfoini.json"
     with open(f"{simDIR}/INIDEF/pphinfoini.json", 'r') as file:
         ini_data = json.load(file)
-    with open(f"{simDIR}/INIDEF/monitors.json", 'r') as file:
-        mon_data = json.load(file)
+    try:
+        with open(f"{simDIR}/INIDEF/monitors.json", 'r') as file:
+            mon_data = json.load(file)
+            num_custom_monitors = len(mon_data["Monitors"])
+    except FileNotFoundError:
+        num_custom_monitors = 0
+
     
    
     default_monitor_center = ini_data["Center Position"]
     default_monitor_size = ini_data["Domain Size"]
     # Find the total number of available monitors
-    num_custom_monitors = len(mon_data["Monitors"])
+    
     if "DFT Plot" in ini_data:
         if ini_data["DFT Plot"] == 0:
             num_default_monitors = 0
@@ -110,20 +116,82 @@ def read_monitor_info(simDIR, v=False):
             print(f"    spans {s[0]}nm along x-axis")
             print(f"    spans {s[1]}nm along y-axis")
             print(f"    spans {s[2]}nm along z-axis")
-        print("========================================================================================================\n")
+        print("========================================================================================================")
         
     return monitor_information_dict
     
+def get_availabe_wavlengths(simDIR):
+    DFTfiles = os.listdir(f"{simDIR}/OUTPUT/DFT")
+    available_wavelength_list = []
+    for fn in DFTfiles:
+        fn = fn.split('nm')
+        fn = fn[0]
+        fn = fn.split('_')
+        if fn[-1] != "Mask.cev" and fn[-1] != "phase.txt":
+            available_wavelength_list.append(int(fn[-1]))
+    available_wavelength_list.sort()
+    #remove duplicate wavelength values
+    available_wavelength_list = list(dict.fromkeys(available_wavelength_list))
+    return available_wavelength_list
+
+def get_nearest_wavelength(wl, available_wavelength_list):
+    wl = min(available_wavelength_list, key=lambda x:abs(x-wl))
+    idx = available_wavelength_list.index(wl)
+    return (wl, idx)
 
 
-
-
-def get_dft_file(simDIR, wavelength):
+def get_dft_file(simDIR, wavelengths, v=False):
     """
         find all of the files in simDIR/OUTPUT/DFT 
         that match the provided wavelength and return
         the filename and coordinate information
     """
+    available_wavelength_list = get_availabe_wavlengths(simDIR)
+    monitor_information_dict = read_monitor_info(simDIR, v)
+    
+    
+    for k in range(len(wavelengths)):
+        w, idx = get_nearest_wavelength(int(wavelengths[k]), available_wavelength_list)
+        wavelengths[k] = w
+
+
+    dft_file_list = []
+    
+    for wl in wavelengths:
+        custom_monitor_index = 1
+        for k in range(len(monitor_information_dict["format"])):
+            f = monitor_information_dict["format"][k-1]
+            p = monitor_information_dict["plane"][k-1]
+            if f == "Custom":
+                fname = f"{simDIR}/OUTPUT/DFT/E_DFT_{custom_monitor_index}_WL_{wl}nm.cev"
+                custom_monitor_index += 1
+            elif f == "Default":
+                fname = f"{simDIR}/OUTPUT/DFT/E_{wl}nm_{p}.cev"
+            dft_dict = {
+                "size": monitor_information_dict['size'][k],
+                "center": monitor_information_dict['center'][k],
+                "type": monitor_information_dict['type'][k],
+                "plane": monitor_information_dict['plane'][k],
+                "dimensions": monitor_information_dict['dimensions'][k],
+                "format": monitor_information_dict['format'][k],
+                "path": fname}
+            dft_file_list.append(dft_dict)
+
+        
+    
+
+
+    if v == True:
+        print("\n========================================================================================================")
+        print("Checking OUTPUT/DFT for valid monitor data at the following wavelengths: ", end='')
+        for wl in wavelengths:
+            print(f"{wl}nm  ", end='')
+        print("\n========================================================================================================")
+        for k in range(len(dft_file_list)):
+            print(f"{dft_file_list[k]['format']} {dft_file_list[k]['plane']}-plane Monitor found at {dft_file_list[k]['path']}")
+        print("========================================================================================================\n")
+
+    return dft_file_list
     
 
 
@@ -141,13 +209,19 @@ if __name__=="__main__":
     parsedArgs = parser.parse_args().__dict__
     simDIR = parsedArgs["simDIR"]
     wl = parsedArgs["wavelength"]
-
+    if simDIR == '.':
+        simDIR = os.getcwd()
+    simDIRsep = simDIR.split('/')
     if len(wl) == 1:
-        print(f"Reading DFT data from {simDIR} with wavelength: \n    {wl[0]}nm")
+        print(f"Reading DFT data from ../{simDIRsep[-3]}/{simDIRsep[-2]}/{simDIRsep[-1]} with wavelength: \n    {wl[0]}nm")
     else:
-        print(f"Reading DFT data from {simDIR} with wavelengths:")
+        print(f"Reading DFT data from ../{simDIRsep[-3]}/{simDIRsep[-2]}/{simDIRsep[-1]} with wavelengths: ", end='')
         for i in range(len(wl)):
-            print(f"    {wl[i]}nm")
+            print(f"{wl[i]}nm   ", end='')
+    print('\n')
 
-    simDIR = parsedArgs["simDIR"]
-    read_monitor_info(simDIR, v=True)
+    #get_availabe_wavlengths(simDIR)
+
+    #simDIR = parsedArgs["simDIR"]
+    #read_monitor_info(simDIR, v=True)
+    get_dft_file(simDIR, wl, v=True)
